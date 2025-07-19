442062ccb7ac34142b7e36eb841f4e2ae8f2ee5a
thomas1y <thomasyohannes15@gmail.com>
2025-07-19 07:38:57 -0700

    new

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,11 +9,13 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      secure: true,        // must be true for HTTPS
+      sameSite: "none",    // allows cross-site cookie sending
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-    }).json({
-        success:true,
-        message,
-        user
+    })
+    .json({
+      success: true,
+      message,
+      user,
     });
 };
